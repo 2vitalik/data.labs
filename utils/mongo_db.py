b1dcb86c07af532e27140e20cc7f4c840727d25a
Vitaliy Lyapota <2vitalik@gmail.com>
2024-11-11 23:46:18 +0200

    mongo: Method to edit entries

--- utils/mongo_db.py
+++ utils/mongo_db.py
@@ -67,5 +67,40 @@ class EntriesTable:
             'prefix': prefix,
         })
 
+    def get_data(self, semester, prefix):
+        entries = self.get(semester, prefix)
+        data = {}
+        for entry in entries:
+            subject = entry.get('subject')
+            category = entry.get('category')
+            title = entry.get('title')
+            teacher = entry.get('teacher')
+            links = entry.get('links')
+            data[f'{subject}|{category}|{title}|{teacher}'] = links
+        return data
+
+    def edit(self, semester, prefix, new_data):
+        old_data = self.get_data(semester, prefix)
+        for key, new_value in new_data.items():
+            if key == 'csrfmiddlewaretoken':
+                continue
+
+            subject, category, title, teacher = key.split('|')
+            old_value = old_data.get(key)
+
+            if old_value != new_value:
+                self.collection.update_one({
+                    'semester': semester,
+                    'prefix': prefix,
+                    'subject': subject,
+                    'category': category,
+                    'title': title,
+                    'teacher': teacher,
+                }, {
+                    '$set': {
+                        'links': new_value,
+                    }
+                })
+
 
 db = MongoDB()
