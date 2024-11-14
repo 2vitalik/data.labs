49df9dc560213823055f3978c6819e5c1c101890
Vitaliy Lyapota <2vitalik@gmail.com>
2024-11-14 18:24:18 +0200

    user-track: Add decorator to track visits

--- src/track_user.py
+++ src/track_user.py
@@ -1,3 +1,7 @@
+from functools import wraps
+
+from utils.mongo_db import db
+
 
 def get_user_args(request):
     return {
@@ -5,3 +9,12 @@ def get_user_args(request):
         'user_ip': request.META.get('REMOTE_ADDR'),
         'user_agent': request.META.get('HTTP_USER_AGENT'),
     }
+
+
+def track_visit(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        full_path = request.get_full_path()
+        db.visits.add(full_path, get_user_args(request))
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
